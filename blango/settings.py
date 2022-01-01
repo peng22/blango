eef8820c3e10bd6cc04ee8fc265a1d3b38f7d7b0
Mohamed Farid <mohamed_mido22107@yahoo.com>
2022-01-01 11:15:57 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -184,5 +184,8 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
-        ]
+        ],
+      "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+         ], 
     }
