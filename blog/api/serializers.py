f0e70248a3eb5808866384506b97840eb480265e
Mohamed Farid <mohamed_mido22107@yahoo.com>
2022-01-01 19:04:19 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -10,6 +10,11 @@ class TagField(serializers.SlugRelatedField):
             self.fail(f"Tag value {data} is invalid")
 
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,15 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail, UserDetail
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Blango API",
@@ -19,8 +23,6 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),  
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
@@ -38,3 +40,12 @@ urlpatterns = [
 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    # ... other patterns omitted
+    path("", include(router.urls)),
+]
