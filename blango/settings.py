dcd983032a3ae9695893282b0fdd911955bfe657
Mohamed Farid <mohamed_mido22107@yahoo.com>
2022-01-03 18:37:22 +0000

    Finish throttling

--- blango/settings.py
+++ blango/settings.py
@@ -189,6 +189,19 @@ class Dev(Configuration):
       "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
          ], 
+    
+      "DEFAULT_THROTTLE_CLASSES": [
+            "blog.api.throttling.AnonSustainedThrottle",
+            "blog.api.throttling.AnonBurstThrottle",
+            "blog.api.throttling.UserSustainedThrottle",
+            "blog.api.throttling.UserBurstThrottle",
+        ],
+  "DEFAULT_THROTTLE_RATES": {
+            "anon_sustained": "500/day",
+            "anon_burst": "10/minute",
+            "user_sustained": "5000/day",
+            "user_burst": "100/minute",
+        },
     }
   SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
@@ -196,3 +209,4 @@ class Dev(Configuration):
             "Basic": {"type": "basic"},
         }
     }
+
